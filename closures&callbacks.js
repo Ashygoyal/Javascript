a7061a038aad39395163726348c44bd8374b707b
ashygoyal <goyal.ashish3@gmail.com>
2018-03-22 00:49:42 +0530

    Added two definitions

--- closures&callbacks.js
+++ closures&callbacks.js
@@ -18,6 +18,17 @@ sayHiLater();
 // other function “otherFunction”) as a parameter, and the callback
 // function is called (or executed) inside the otherFunction.
 
+// Higher Order Function: A higher-order function (also functional, functional form or functor) 
+// is a function that does at least one of the following: takes one or 
+// more functions as arguments (i.e., procedural parameters), returns 
+// a function as its result
+
+// First class function: A programming language is said to have 
+// First-class functions when functions in that language are treated 
+// like any other variable. For example, in such a language, a function 
+// can be passed as an argument to other functions, can be returned by 
+// another function and can be assigned as a value to a variable. 
+
 function tellMeWhenDone(callback) {
   var a = 1000;
   var b = 2000;
