59c42ed75d452804794964fb55cc183446540e34
ashygoyal <goyal.ashish3@gmail.com>
2018-04-05 23:50:50 +0530

    Difference between classical and prototypal inheritance

---
+++ prototypal_Inheritance.js
@@ -0,0 +1,7 @@
+// Inheritance (in Javascript): One object gets access to the properties
+// and methods of another object
+
+// Classical Inheritance: The way inheritance is implemented in C++, Java, etc
+// Its very verbose, involves lot of concepts and keywords like: friend, protected, private, interface
+
+// Prototypal Inheritance: It is simple, flexible, extensible, easy to understand
