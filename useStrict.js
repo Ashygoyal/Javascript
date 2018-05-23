4464401097f01a304b94bd7c80670bb64fa26bc7
ashygoyal <goyal.ashish3@gmail.com>
2018-05-24 01:39:34 +0530

    s7v67 - use strict mode in Javascript

---
+++ useStrict.js
@@ -0,0 +1,26 @@
+// In Detail: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode
+
+// use strict can be the first line of a js file or first line of
+// a function
+
+var person;
+
+persom = {};
+console.log(persom);
+
+
+// ******************************** 
+
+function demoFunc(){
+  "use strict";
+   var person1;
+  
+  persom1 ={}; 
+  console.log(persom1);
+}
+
+demoFunc();  // Uncaught ReferenceError: persom1 is not defined
+
+// If we have 5 js files. First file has 'use strict'. We concatenate
+// and minify all 5 files into a single file, then 'use strict' will be
+// set for all 5 files. Be careful there.
