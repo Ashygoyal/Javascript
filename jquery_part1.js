3aaabe8a871779c9c29d58d83157c55f416718ae
ashygoyal <goyal.ashish3@gmail.com>
2018-06-10 18:45:19 +0530

    Jquery deep dive part 1

---
+++ jquery_part1.js
@@ -0,0 +1,6 @@
+// jQuery doesnt add any extra feature to the browser or Javascript, it handles the browser's quirks.
+// It lets you manipulate the DOM inside a browser. Its separate from Javascript engine. It looks at
+// html and decides how to render it on screen. A tree like structure that stores a representation
+// of the html. jQuery makes it easy to look at DOM to find elements on page and manipulate them.
+
+var q =$();
